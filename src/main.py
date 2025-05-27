3e7141241d298b8eb4d1de0654964d251beb860f
FedeLupianez <lupianezfederico@gmail.com>
2025-05-27 17:06:36 -0300

    Add basic window at main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
-# Archivo principal del juego, hay que tratar de que este archivo no contenga 
+# Archivo principal del juego, hay que tratar de que este archivo no contenga
 # demasiadas líneas, lo ideal sería que se importen los componentes y los personajes acá
+import arcade
+import constants
 
+from scenes.View import View
+
+main_view = View()
+
+if __name__ == "__main__":
+    arcade.run(main_view)
