8f42a42d3766a02ea0322e11df9342a46b0daed4
FedeLupianez <lupianezfederico@gmail.com>
2025-05-27 19:01:08 -0300

    Create new data manager file to data managment

---
+++ src/DataManager.py
@@ -0,0 +1,3 @@
+# En este archivo vamos a poder manejar la lógica detras del guardado de datos.
+# Por ejemplo, para guardar el nivel  items que tiene el usuario si se sale del juego
+# o al cambiar de escena, aula, etc.
