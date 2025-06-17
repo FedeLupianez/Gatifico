560044fbdf186d5205f4a02e02d77d2f1457d3ab
FedeLupianez <lupianezfederico@gmail.com>
2025-06-16 21:38:48 -0300

    Comento el codigo, mejoro la movilidad del personaje y la carga de sprites

--- src/characters/Player.py
+++ src/characters/Player.py
@@ -1,106 +1,142 @@
 import arcade
-from Constants import Game
+from Constants import Game, AssetsUrls
 from StateMachine import StateMachine
 
 
 IdlePath = "src/assets/2D Pixel Dungeon Asset Pack/Character_animation/monsters_idle/skeleton2/v2/skeleton2_v2_{}.png"
 MovementPath = "src/assets/Enemy_Animations_Set/enemies-vampire_movement.png"
 
+# Defino los id de los estados para no repetir magic strings
+IDLE = "IDLE"
+LEFT = "LEFT"
+RIGHT = "RIGHT"
+UP = "UP"
+DOWN = "DOWN"
+
 
 class Player(StateMachine):
     def __init__(self):
         super().__init__()
         self.motions = [arcade.key.W, arcade.key.A, arcade.key.S, arcade.key.D]
-        self.actualAnimationPath: str = IdlePath.replace("{}", "1")
-        self.sprite = arcade.Sprite(self.actualAnimationPath)
+        # Todos los path tienen llaves {} donde iría el numero de sprite
+        self.actualAnimationPath: str = IdlePath.replace(
+            "{}",
+            str(
+                AssetsUrls.INITIAL_INDEX
+            ),  # como es el sprite inicial lo intercambiamos por el indice inicial
+        )
+        self.sprite: arcade.Sprite = arcade.Sprite(
+            self.actualAnimationPath
+        )  # objeto sprite del personaje
         self.speed = Game.PLAYER_SPEED
-        self.actualAnimationFrames: int = 0
-        self.frames: list[arcade.Texture] = []
-        self.textureIndex = 0
-        self.animationTimer: float = 0.0
+        self.actualAnimationFrames: int = 0  # cantidad de frames de la animacion
+        self.frames: list[arcade.Texture] = []  # lista de texturas
+        self.textureIndex = 0  # indice actual de la textura
+        self.animationTimer: float = 0.0  # timer de la animacion
         self.sprite.scale = 5
-        self.spriteCache: dict[str, arcade.Texture] = {}
+        self.spriteCache: dict[
+            str, arcade.Texture
+        ] = {}  # Diccionario con las texturas cargadas para ahorrar llamdas a memoria
 
-    def handleEvent(self, event: int):
-        if event < 0:
-            return "IDLE"
-
-        match event:
+    def handleMovementEvent(self, key: int):
+        """Función que se llama siempre que se toca una tecla de movimiento
+        Args :
+            key (int) : Tecla presionada
+        """
+        # Si la key es negativa significa que se soltó una tecla,
+        # por lo que regresa al estado IDLE
+        if key < 0:
+            return IDLE
+        # Si es que presionó una tecla, se cambia al estado correspondiente
+        match key:
             case arcade.key.W:
-                return "UP"
+                return UP
             case arcade.key.A:
-                return "LEFT"
+                return LEFT
             case arcade.key.S:
-                return "DOWN"
+                return DOWN
             case arcade.key.D:
-                return "RIGHT"
+                return RIGHT
             case _:
-                return self.actualState
+                # Si no es ninguna de las otras teclas retorna el estado actual
+                return self.actualStateId
 
+    # Definición de los estados como funciones
     def IdleState(self, event):
         self.sprite.change_x = 0
         self.sprite.change_y = 0
         self.actualAnimationPath = IdlePath
         self.actualAnimationFrames = 4
-        self.updateSpriteList()
-        return self.handleEvent(event)
+        return self.handleMovementEvent(event)
 
     def LeftState(self, event):
         self.sprite.change_x = -Game.PLAYER_SPEED
         self.sprite.change_y = 0
         self.actualAnimationPath = IdlePath
         self.actualAnimationFrames = 4
-        self.updateSpriteList()
-        return self.handleEvent(event)
+        self.sprite.scale_x = -abs(self.sprite.scale_x)
+        return self.handleMovementEvent(event)
 
     def RightState(self, event):
         self.sprite.change_x = Game.PLAYER_SPEED
         self.sprite.change_y = 0
         self.actualAnimationPath = IdlePath
         self.actualAnimationFrames = 4
-        self.updateSpriteList()
-        return self.handleEvent(event)
+        self.sprite.scale_x = abs(self.sprite.scale_x)
+        return self.handleMovementEvent(event)
 
     def DownState(self, event):
         self.sprite.change_y = -Game.PLAYER_SPEED
         self.sprite.change_x = 0
         self.actualAnimationPath = IdlePath
         self.actualAnimationFrames = 4
-        self.updateSpriteList()
-        return self.handleEvent(event)
+        return self.handleMovementEvent(event)
 
     def UpState(self, event):
         self.sprite.change_y = Game.PLAYER_SPEED
         self.sprite.change_x = 0
         self.actualAnimationPath = IdlePath
-        self.updateSpriteList()
-        return self.handleEvent(event)
+        return self.handleMovementEvent(event)
 
     def setup(self):
-        self.addState("IDLE", self.IdleState)
-        self.addState("LEFT", self.LeftState)
-        self.addState("RIGHT", self.RightState)
-        self.addState("DOWN", self.DownState)
-        self.addState("UP", self.UpState)
+        """Función para configurar la maquina de estados del personaje"""
+        self.addState(IDLE, self.IdleState)
+        self.addState(LEFT, self.LeftState)
+        self.addState(RIGHT, self.RightState)
+        self.addState(DOWN, self.DownState)
+        self.addState(UP, self.UpState)
 
     def updatePosition(self):
+        """Actualiza la posición del personaje segun la velocidad actual"""
         self.sprite.center_x += self.sprite.change_x
         self.sprite.center_y += self.sprite.change_y
 
     def updateState(self, event: int):
-        self.processState(event)
-        print("Estado actual : ", self.actualState)
+        """Actualiza el estado actual del personaje"""
+        tempState = self.actualStateId
+        self.processState(event)  # Procesa el estado
+        if self.actualStateId != tempState:
+            self.updateSpriteList()
 
     def updateSpriteList(self):
-        for i in range(1, self.actualAnimationFrames + 1):
+        """
+        Actualiza la lista de texturas segun el estado actual
+        """
+        print("cargando nuevas texturas ...\n")
+        self.frames.clear()
+        for i in range(
+            AssetsUrls.INITIAL_INDEX,
+            self.actualAnimationFrames + AssetsUrls.INITIAL_INDEX,
+        ):
             route = self.actualAnimationPath.replace("{}", str(i))
+            # Si el path de la textura ya estaba cargada no hay que cargarla de nuevo
             if route in self.spriteCache.keys():
-                print("esta textura ya esta cargada")
+                # Agrega la textura de la cache a la lista de frames
                 self.frames.append(self.spriteCache[route])
-                pass
-            else:
-                self.spriteCache[route] = arcade.load_texture(route)
-        self.textureIndex = 1
+                continue
+            # Si no está en la cache, la carga y la guarda en ella
+            self.spriteCache[route] = arcade.load_texture(route)
+        self.textureIndex = 0
 
     def updateFrame(self):
         self.sprite.texture = self.frames[self.textureIndex]
@@ -111,5 +147,5 @@ class Player(StateMachine):
         self.animationTimer += deltaTime
         if self.animationTimer > 0.1:
             self.animationTimer = 0
-            self.textureIndex = (self.textureIndex + 1) % len(self.frames)
+            self.textureIndex = (self.textureIndex + 1) % self.actualAnimationFrames
             self.updateFrame()
--- src/scenes/House.py
+++ src/scenes/House.py
@@ -1,47 +1,58 @@
 import arcade
-import scenes.View as ViewScene
+from scenes.View import View
 import Constants
 from characters.Player import Player
+from typing import Callable
 
 
-class House(ViewScene.View):
-    def __init__(self, callback) -> None:
-        backgroundUrl = Constants.AssetsUrls.SCHOOL_BACKGROUND
+class House(View):
+    def __init__(self, callback: Callable) -> None:
+        backgroundUrl = Constants.AssetsUrls.HOUSE_BACKGROUND
         tileMapUrl = None
         super().__init__(backgroundUrl=backgroundUrl, tileMapUrl=tileMapUrl)
         self.window.set_mouse_visible(False)
         self.callback = callback
-        self.playerList = arcade.SpriteList()
-        self.player = Player()
+        self.playerList: arcade.SpriteList = (
+            arcade.SpriteList()
+        )  # Lista de sprites del jugador
+        self.player = Player()  # Defino el personaje
         self.player.sprite.center_x = Constants.Game.SCREEN_WIDTH // 2
         self.player.sprite.center_y = Constants.Game.SCREEN_HEIGHT // 2
         self.player.setup()
-        print(self.player.actualAnimationPath)
         self.playerList.append(self.player.sprite)
+        self.keysPressed: set = set()
 
     def on_show_view(self) -> None:
         return super().on_show_view()
 
     def on_draw(self) -> bool | None:
-        self.clear()
-        self.scene.draw()
+        # Función que se llama cada vez que se dibuja la escena
+        self.clear()  # limpia la pantalla
+        self.scene.draw()  # dibuja la escena
         text = arcade.Text(
             "Casa", Constants.Game.SCREEN_WIDTH / 2, Constants.Game.SCREEN_HEIGHT / 2
         )
         text.draw()
-        self.playerList.draw(pixelated=True)
-
-    def on_update(self, delta_time: float) -> bool | None:
-        self.player.update_animation(delta_time)
-        self.player.updatePosition()
+        self.playerList.draw(pixelated=True)  # dibuja el personaje
 
     def on_key_press(self, symbol: int, modifiers: int) -> bool | None:
         if symbol == arcade.key.SPACE:
             self.callback(Constants.SignalCodes.CHANGE_VIEW, "MENU")
-
-        if symbol in self.player.motions:
-            self.player.updateState(symbol)
+        self.keysPressed.add(symbol)
 
     def on_key_release(self, symbol: int, modifiers: int) -> bool | None:
-        if symbol in self.player.motions:
-            self.player.updateState(-symbol)
+        self.keysPressed.discard(symbol)
+
+    def on_update(self, delta_time: float) -> bool | None:
+        self.player.update_animation(delta_time)
+        self.player.updatePosition()
+        if arcade.key.W in self.keysPressed:
+            self.player.updateState(arcade.key.W)
+        elif arcade.key.A in self.keysPressed:
+            self.player.updateState(arcade.key.A)
+        elif arcade.key.S in self.keysPressed:
+            self.player.updateState(arcade.key.S)
+        elif arcade.key.D in self.keysPressed:
+            self.player.updateState(arcade.key.D)
+        else:
+            self.player.updateState(-1)
