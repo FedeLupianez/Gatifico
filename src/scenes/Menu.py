6fc8e737d42ea8d9c669d002799d98b3d47d20d8
FedeLupianez <lupianezfederico@gmail.com>
2025-07-18 21:45:52 -0300

    Agrego assets para el menu y los pongo en la escena

--- src/scenes/Menu.py
+++ src/scenes/Menu.py
@@ -2,22 +2,22 @@ import arcade
 from Constants import Game, SignalCodes
 from scenes.View import View
 
-StartButtonPath: str = ":resources:UI/StartButton.png"
+StartButtonPath: str = ":resources:UI/PlayButton.png"
 ExitButtonPath: str = ":resources:UI/ExitButton.png"
 
 
 class Menu(View):
     def __init__(self, callback):
-        backgroundUrl = ":resources:Background/Texture/TX Plant.png"
+        backgroundUrl = ":resources:UI/MenuBackground.jpg"
         super().__init__(backgroundUrl=backgroundUrl, tileMapUrl=None)
 
         self.window.set_mouse_visible(True)
         self.spriteList = arcade.SpriteList()
-        self.startButton = arcade.Sprite(StartButtonPath, scale=5)
+        self.startButton = arcade.Sprite(StartButtonPath, scale=2)
         self.startButton.center_x = Game.SCREEN_WIDTH // 2
         self.startButton.center_y = Game.SCREEN_HEIGHT // 2
 
-        self.exitButton = arcade.Sprite(ExitButtonPath, scale=5)
+        self.exitButton = arcade.Sprite(ExitButtonPath, scale=2)
         self.exitButton.center_x = Game.SCREEN_WIDTH // 2
         self.exitButton.center_y = (Game.SCREEN_HEIGHT // 2) - 100
         self.spriteList.append(self.startButton)
@@ -33,18 +33,15 @@ class Menu(View):
         self.spriteList.draw(pixelated=True)
 
     def on_key_press(self, symbol: int, modifiers: int) -> bool | None:
-        match symbol:
-            case arcade.key.SPACE:
-                self.callback(SignalCodes.CHANGE_VIEW, "TEST")
-            case arcade.key.M:
-                self.callback(SignalCodes.CHANGE_VIEW, "MIX_TABLE")
-            case arcade.key.C:
-                self.callback(
-                    SignalCodes.CHANGE_VIEW,
-                    ("CHEST", "chest_1", {"rubi": 4, "rock": 2}),
-                )
-            case arcade.key.S:
-                self.callback(SignalCodes.CHANGE_VIEW, "SPLIT_TABLE")
+        data_to_send = {
+            arcade.key.SPACE: "TEST",
+            arcade.key.M: "MIX_TABLE",
+            arcade.key.C: ("CHEST", "chest_1", {"rubi": 4, "rock": 2}),
+            arcade.key.S: "SPLIT_TABLE",
+        }
+        result = data_to_send.get(symbol, None)
+        if result:
+            self.callback(SignalCodes.CHANGE_VIEW, result)
 
     def on_mouse_press(
         self, x: int, y: int, button: int, modifiers: int
